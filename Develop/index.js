5b1bb705a7ab7d347c5883f5e4e9790cafe9cc88
Trent Roesbery <xtrent777x@gmail.com>
2021-08-06 16:32:37 -0600

    base to questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,41 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: '',
+            name: '',
+            message 'What is your project title?',
+        }
+        {
+            type: '',
+            name: '',
+            message 'Enter your project description.',
+        }
+        {
+            type: 'list',
+            name: '',
+            message 'What license was used for this project?',
+        }
+        {
+            type: '',
+            name: '',
+            message 'What is your GitHub username?', //needs link as well to Github??
+        }
+        {
+            type: '',
+            name: '',
+            message 'What is your email address?',
+        }
+
+
+
+
+    ])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

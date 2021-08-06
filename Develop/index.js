1097e2c5be1e62b2f814f9e725699df5b2d9421d
Trent Roesbery <xtrent777x@gmail.com>
2021-08-06 16:40:03 -0600

    more question work

--- Develop/index.js
+++ Develop/index.js
@@ -3,45 +3,47 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const promptUser = () => {
+const init = () => {
     return inquirer.prompt([
         {
-            type: '',
-            name: '',
-            message 'What is your project title?',
-        }
+            type: 'input',
+            name: 'project title',
+            message: 'What is your project title?',
+        },
         {
-            type: '',
-            name: '',
-            message 'Enter your project description.',
-        }
+            type: 'input',
+            name: 'description',
+            message: 'Enter your project description.',
+        },
         {
             type: 'list',
-            name: '',
-            message 'What license was used for this project?',
-        }
+            name: 'license',
+            message: 'What license was used for this project?',
+        },
         {
-            type: '',
-            name: '',
-            message 'What is your GitHub username?', //needs link as well to Github??
-        }
+            type: 'input',
+            name: 'githubName',
+            message: 'What is your GitHub username?', //needs link as well to Github??
+        },
         {
-            type: '',
-            name: '',
-            message 'What is your email address?',
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
         }
 
 
 
 
-    ])
-}
+    ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+//function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+
+//function init() {}
 
 // Function call to initialize app
 init();
